f3c4f4b52fcc791c5e75562d7ec6146b99a5f283
Shayan Faghihi <s.faghihi@bettechnology.com>
2023-09-17 12:36:17 +0330

    edited container className

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from "react";
 
 interface ContainerProps {
-  className?: string;
+  className: string;
   children: ReactNode;
 }
 
 const Container: React.FC<ContainerProps> = ({ className, children }) => {
   return (
     <div
-      className={"w-[80%] max-w-5xl mx-auto flex flex-col gap-8" + className}
+      className={`w-[80%] max-w-5xl mx-auto flex flex-col gap-8 ${className}`}
     >
       {children}
     </div>
