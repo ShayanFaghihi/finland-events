1358f30260f51eeac506183abb062ad1791aae36
Shayan Faghihi <s.faghihi@bettechnology.com>
2023-09-17 16:19:36 +0330

    edited container classname logic

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -8,7 +8,9 @@ interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({ className, children }) => {
   return (
     <div
-      className={`w-[80%] max-w-5xl mx-auto flex flex-col gap-8 ${className}`}
+      className={`w-[80%] max-w-5xl mx-auto flex flex-col gap-8 ${
+        className || ""
+      }`}
     >
       {children}
     </div>
